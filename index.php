3241e5ec935c1041be65b35eb7e803bff20daaf5
Mike <25435547+mxgtech@users.noreply.github.com>
2018-07-04 00:45:20 +0800

    index.php uikit

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-<?php echo 'Hello Serve'; ?>
+<?php 
+require('inc/config.php');
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHPloginSystem</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+	<!-- UIkit CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/css/uikit.min.css" />
+</head>
+<body>
+	<div class="uk-section uk-container uk-preserve-color uk-align-center">
+
+		<div class="uk-text-center uk-align-center">
+			<h2>Welcome to MatrixPortal</h2>
+		</div>
+
+		<div class="uk-grid uk-child-width-1-3@m uk-child-width-1-1 " uk-grid>
+
+			<form class="uk-form-stacked js-login uk-align-center uk-child-width-* " >
+
+		       <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com">
+			        </div>
+			    </div>
+		       <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your password">
+			        </div>
+			    </div>
+		      <div class="uk-margin">
+			       <button class="uk-button uk-button-primary">Login</button>
+			    </div>
+			</form>
+		</div>
+
+	</div>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/js/uikit-icons.min.js"></script>
+</body>
+</html>
